6003d4e47ca780832d6c3ee1d3da3350f9146aef
musuko <100979661+musuko@users.noreply.github.com>
2022-07-18 21:00:23 +0900

    Add files via upload
    index2.php: 4行目、5行目に、htmlspecialcharsを短くした関数と、データベースに接続する命令をrequireで読み込む命令を追加。

--- index2.php
+++ index2.php
@@ -1,4 +1,6 @@
 <?php require 'header.php'; ?>
 <?php require 'menu2.php'; ?>
+<?php require 'common.php'; ?>
+<?php require 'dbconnect.php'; ?>
 <?php require 'top2.php'; ?>
 <?php require 'footer.php'; ?>
